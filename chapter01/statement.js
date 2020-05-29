0fb8f71e83b1fcf805ee22c8cf242b5e6a1a7800
leee-dw <dylan.dw.lee@gmail.com>
2020-05-29 13:25:44 +0900

    중간 점검: 난무하는 중첩 함수

--- chapter01/statement.js
+++ chapter01/statement.js
@@ -2,6 +2,55 @@ const invoices = require('./invoices.json')
 const plays = require('./plays.json')
 
 function statement(invoice, plays) {
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`
+
+  for (let perf of invoice.performances) {
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
+      perf.audience
+    }석) \n`
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`
+  return result
+
+  function totalAmount() {
+    let result = 0
+    for (let perf of invoice.performances) {
+      result += amountFor(perf)
+    }
+    return result
+  }
+
+  function totalVolumeCredits() {
+    let result = 0 // 변수 선언을 반복문 앞으로 이동
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf) // 값 누적 로직을 별도 for문으로 분리
+    }
+    return result
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100)
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0
+    result += Math.max(aPerformance.audience - 30, 0)
+    if ('comedy' === playFor(aPerformance).type) {
+      result += Math.floor(aPerformance.audience / 5)
+    }
+    return result
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playId]
+  }
+
   function amountFor(aPerformance) {
     // 필요 없어진 매개변수 제거
     let result = 0
@@ -29,57 +78,6 @@ function statement(invoice, plays) {
     }
     return result
   }
-  function playFor(aPerformance) {
-    return plays[aPerformance.playId]
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if ('comedy' === playFor(aPerformance).type) {
-      result += Math.floor(aPerformance.audience / 5)
-    }
-    return result
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100)
-  }
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0 // 변수 선언을 반복문 앞으로 이동
-    for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf) // 값 누적 로직을 별도 for문으로 분리
-    }
-    return volumeCredits
-  }
-
-  function appleSauce() {
-    let totalAmount = 0
-    for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf)
-    }
-    return totalAmount
-  }
-
-  let result = `청구 내역 (고객명: ${invoice.customer})\n`
-  let totalAmount = 0
-
-  for (let perf of invoice.performances) {
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
-      perf.audience
-    }석) \n`
-  }
-
-  let totalAmount = appleSauce()
-
-  result += `총액: ${usd(totalAmount)}\n`
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`
-  return result
 }
 
 console.log(statement(invoices, plays))
