c652f76ca8930a64805de65bc7f731ff6bcff011
leee-dw <dylan.dw.lee@gmail.com>
2020-06-01 23:18:05 +0900

    함수들을 계산기로 옮기기

--- chapter01/src/js/createStatementData.js
+++ chapter01/src/js/createStatementData.js
@@ -3,6 +3,40 @@ class PerformanceCalculator {
     this.performance = aPerformance
     this.play = aPlay
   }
+
+  get amount() {
+    let result = 0
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30)
+        }
+        break
+
+      case 'comedy':
+        result = 30000
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20)
+        }
+
+        result += 300 * this.performance.audience
+        break
+
+      default:
+        throw new Error(`알 수 없는 장르: ${this.play.type}`)
+    }
+    return result
+  }
+
+  get volumeCreditsFor() {
+    let result = 0
+    result += Math.max(aPerformance.audience - 30, 0)
+    if ('comedy' === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5)
+    }
+    return result
+  }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -18,11 +52,12 @@ export default function createStatementData(invoice, plays) {
       aPerformance,
       playFor(aPerformance)
     )
+
     const result = Object.assign({}, aPerformance)
 
     result.play = calculator.play
-    result.amount = amountFor(result)
-    result.volumeCredits = volumeCreditsFor(result)
+    result.amount = calculator.amount
+    result.volumeCredits = calculator.volumeCredits
     return result
   }
 
@@ -31,38 +66,10 @@ export default function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-
-      case 'comedy':
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-
-        result += 300 * aPerformance.audience
-        break
-
-      default:
-        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`)
-    }
-    return result
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
   }
 
-  function volumeCreditsFor(aPerformance) {
-    let result = 0
-    result += Math.max(aPerformance.audience - 30, 0)
-    if ('comedy' === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5)
-    }
-    return result
-  }
+  function volumeCreditsFor(aPerformance) {}
 
   function totalAmount(data) {
     return data.performances.reduce((total, p) => total + p.amount, 0)
